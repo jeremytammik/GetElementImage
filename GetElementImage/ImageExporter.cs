1df8c3d0714b4f988820c15abd7918d53bc21bb5
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-07-06 12:11:05 +0200

    implemented GetOrientationFor and support for exporting list of views

--- GetElementImage/ImageExporter.cs
+++ GetElementImage/ImageExporter.cs
@@ -12,9 +12,18 @@ namespace GetElementImage
   class ImageExporter
   {
     /// <summary>
-    /// View directions to export
+    /// View points to export
+    /// distance -- from element centre (defined by the bounding box).
+    /// yaw -- horizontal angle, 0-360 degrees; 0 means looking North, 180 means looking South
+    /// pitch -- vertical angle, -90 to +90 degrees, 0 being on the same height as the element centre and 90 looking from the top
     /// </summary>
-
+    object[][] _view_data_to_export =
+    {
+      new object[] { "Isometric", 1, 45, 35.264 },
+      new object[] { "North", 1, 0, 0 },
+      new object[] { "East", 1, 90, 0},
+      new object[] { "Top", 1, 0, 90, 0 }
+    };
 
     List<View> _views_to_export;
     //List<ElementId> _category_ids_to_hide;
@@ -30,6 +39,50 @@ namespace GetElementImage
       BuiltInCategory.OST_ProjectBasePoint
     };
 
+    ViewOrientation3D GetOrientationFor( 
+      double yaw_degrees, 
+      double pitch_degrees )
+    {
+      // Yaw = 0 means north; rotate by 90 degrees, 
+      // so that north is 90 degrees instead
+
+      if( Util.IsEqual( 270, yaw_degrees ) 
+        || 270 > yaw_degrees )
+      {
+        yaw_degrees += 90.0;
+      }
+
+      double angle_in_xy_plane = yaw_degrees * Math.PI / 180.0;
+      double angle_up_down = pitch_degrees * Math.PI / 180.0;
+      //double radius = 1;
+
+      // Eye position is arbitrary, since it is defined 
+      // by fitting the view to the selected element
+
+      XYZ eye = new XYZ(
+        Math.Cos( angle_in_xy_plane ),
+        Math.Sin( angle_in_xy_plane ),
+        Math.Cos( angle_up_down ) );
+
+      XYZ forward = -eye;
+
+      XYZ left = Util.IsVertical( forward )
+        ? -XYZ.BasisX
+        : XYZ.BasisZ.CrossProduct( forward );
+
+      XYZ up = forward.CrossProduct( left );
+
+      // Setting ùp`to the Z axis, XYZ.BasisZ, throws
+      // Autodesk.Revit.Exceptions.ArgumentsInconsistentException:
+      // The vectors upDirection and forwardDirection 
+      // are not perpendicular.
+
+      ViewOrientation3D orientation
+        = new ViewOrientation3D( eye, up, forward );
+
+      return orientation;
+    }
+
     public ImageExporter( Document doc )
     {
       ViewFamilyType viewFamilyType
@@ -41,55 +94,75 @@ namespace GetElementImage
 
       Debug.Assert( null != viewFamilyType );
 
-      View3D view3d = View3D.CreateIsometric( 
+      View3D v = View3D.CreateIsometric( 
         doc, viewFamilyType.Id );
 
-      view3d.Name = "Isometric";
+      v.Name = "Isometric";
 
-      Debug.Assert( null != view3d );
+      Debug.Assert( null != v );
 
-      _views_to_export = new List<View>( 1 ) { view3d };
+      int nViews = _view_data_to_export.Length;
 
-      Parameter graphicDisplayOptions
-        = view3d.get_Parameter(
-          BuiltInParameter.MODEL_GRAPHICS_STYLE );
+      _views_to_export = new List<View>( nViews ) { v };
 
-      // Settings for best quality
-
-      graphicDisplayOptions.Set( 6 );
-
-      // Get categories to hide
-
-      //_category_ids_to_hide = new List<ElementId>( 2 );
-
-      Categories cats = doc.Settings.Categories;
-
-      //_category_ids_to_hide.Add( cats.get_Item( // null object
-      //  BuiltInCategory.OST_Cameras ).Id );
-      //_category_ids_to_hide.Add( cats.get_Item( 
-      //  BuiltInCategory.OST_Levels ).Id );
-      //_category_ids_to_hide.Add( cats.get_Item( 
-      //  BuiltInCategory.OST_ProjectBasePoint ).Id );
-
-      foreach( BuiltInCategory bic in _categories_to_hide )
+      for( int i = 1; i < nViews; ++i )
       {
-        Category cat = cats.get_Item( bic );
+        v = View3D.CreateIsometric(
+          doc, viewFamilyType.Id );
 
-        if( null == cat )
-        {
-          Debug.Print( "{0} returns null category.", bic );
-        }
-        else
-        {
-          view3d.SetCategoryHidden( cat.Id, true );
-        }
+        object[] d = _view_data_to_export[ i ];
 
-        // BuiltInCategory.OST_Cameras throws exception:
-        // Autodesk.Revit.Exceptions.ArgumentException
-        // Category cannot be hidden.
-        // Parameter name: categoryId
-        //ElementId id = new ElementId( bic );
-        //view3d.SetCategoryHidden( id, true );
+        v.Name = d[ 0 ] as string;
+        v.SetOrientation( GetOrientationFor( 
+          (double)(d[ 2 ]), (double) (d[ 3 ]) ) );
+        v.SaveOrientation();
+
+        _views_to_export.Add( v );
+      }
+
+      foreach( View v2 in _views_to_export )
+      {
+        Parameter graphicDisplayOptions
+          = v2.get_Parameter(
+            BuiltInParameter.MODEL_GRAPHICS_STYLE );
+
+        // Settings for best quality
+
+        graphicDisplayOptions.Set( 6 );
+
+        // Get categories to hide
+
+        //_category_ids_to_hide = new List<ElementId>( 2 );
+
+        Categories cats = doc.Settings.Categories;
+
+        //_category_ids_to_hide.Add( cats.get_Item( // null object
+        //  BuiltInCategory.OST_Cameras ).Id );
+        //_category_ids_to_hide.Add( cats.get_Item( 
+        //  BuiltInCategory.OST_Levels ).Id );
+        //_category_ids_to_hide.Add( cats.get_Item( 
+        //  BuiltInCategory.OST_ProjectBasePoint ).Id );
+
+        foreach( BuiltInCategory bic in _categories_to_hide )
+        {
+          Category cat = cats.get_Item( bic );
+
+          if( null == cat )
+          {
+            Debug.Print( "{0} returns null category.", bic );
+          }
+          else
+          {
+            v2.SetCategoryHidden( cat.Id, true );
+          }
+
+          // BuiltInCategory.OST_Cameras throws exception:
+          // Autodesk.Revit.Exceptions.ArgumentException
+          // Category cannot be hidden.
+          // Parameter name: categoryId
+          //ElementId id = new ElementId( bic );
+          //view3d.SetCategoryHidden( id, true );
+        }
       }
     }
 
@@ -97,43 +170,27 @@ namespace GetElementImage
     {
       Document doc = e.Document;
 
-      // Hide all other elements in view
+      // Hide all other elements in views to export
 
-      View view = _views_to_export[ 0 ];
+      foreach( View v in _views_to_export )
+      {
 
-      List<ElementId> hideable_element_ids
-        = new FilteredElementCollector( doc, view.Id )
-          .Where<Element>( a => a.CanBeHidden( view ) )
-          .Select<Element, ElementId>( b => b.Id )
-          .ToList<ElementId>();
+        List<ElementId> hideable_element_ids
+          = new FilteredElementCollector( doc, v.Id )
+            .Where<Element>( a => a.CanBeHidden( v ) )
+            .Select<Element, ElementId>( b => b.Id )
+            .ToList<ElementId>();
 
-      view.HideElements( hideable_element_ids );
+        v.HideElements( hideable_element_ids );
 
-      //view.HideCategoriesTemporary( _category_ids_to_hide );
+        //v.HideCategoriesTemporary( _category_ids_to_hide );
 
-      List<ElementId> ids = new List<ElementId>( 1 ) { e.Id };
+        List<ElementId> ids = new List<ElementId>( 1 ) { e.Id };
 
-      view.UnhideElements( ids );
+        v.UnhideElements( ids );
+      }
 
       doc.Regenerate();
-
-      //string directory = Path.GetTempPath();
-
-      //string tempFileName = Path.ChangeExtension(
-      //  Path.GetRandomFileName(), "png" );
-
-      //string tempImageFile;
-
-      //try
-      //{
-      //  tempImageFile = Path.Combine(
-      //    directory, tempFileName );
-      //}
-      //catch( IOException )
-      //{
-      //  return null;
-      //}
-
 
       string dir = "C:/tmp";
       string fn = e.Id.IntegerValue.ToString();
---
+++ GetElementImage/Util.cs
@@ -0,0 +1,41 @@
+﻿using Autodesk.Revit.DB;
+using System;
+
+namespace GetElementImage
+{
+  class Util
+  {
+    #region Geometrical Comparison
+    public const double _eps = 1.0e-9;
+
+    public static double Eps
+    {
+      get
+      {
+        return _eps;
+      }
+    }
+
+    public static bool IsZero(
+      double a,
+      double tolerance = _eps )
+    {
+      return tolerance > Math.Abs( a );
+    }
+
+    public static bool IsEqual(
+      double a,
+      double b,
+      double tolerance = _eps )
+    {
+      return IsZero( b - a, tolerance );
+    }
+
+    public static bool IsVertical( XYZ v )
+    {
+      return IsZero( v.X ) && IsZero( v.Y );
+    }
+    #endregion // Geometrical Comparison
+
+  }
+}
